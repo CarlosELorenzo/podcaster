f0c9e29b622e5b18217a3efe703137e6f32833eb
CarlosELorenzo <ce.lorenzol@gmail.com>
2023-07-11 17:53:34 +0200

    feat: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import { Home, Podcast, Episode } from "./pages";
 
+import { Header } from "./coponents";
+
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/podcast/:podcastId" element={<Podcast />} />
-        <Route
-          path="/podcast/:podcastId/episode/:episodeId"
-          element={<Episode />}
-        />
-      </Routes>
+      <div className="container max-w-[80%] mx-auto px-4  w-full">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/podcast/:podcastId" element={<Podcast />} />
+          <Route
+            path="/podcast/:podcastId/episode/:episodeId"
+            element={<Episode />}
+          />
+        </Routes>
+      </div>
     </>
   );
 }
